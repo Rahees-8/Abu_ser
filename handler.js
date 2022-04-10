26038ce5d1844509860b2fcc61f6b74e0e763f42
Rahees-8 <103293864+Rahees-8@users.noreply.github.com>
2022-04-10 17:04:31 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+917025994178' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Abu-ser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+917994206751' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? '𝙍𝘼𝙃𝙀𝙀𝙎' : process.env.DEPLOYER,    
 };
